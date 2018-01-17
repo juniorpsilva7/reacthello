88902929b9b572a3408dc7c37d10dfd037c0bd48
Junior Silva <juniorpsilva7@gmail.com>
2018-01-17 15:38:13 -0200

    Hello finish

--- app/App.js
+++ app/App.js
@@ -1,5 +1,6 @@
 var React = require('react'); // importa a lib do react
 var reactDOM = require('react-dom'); // importa a lib react-dom
 
+var MeuComponente = require('./components/MeuComponente.js');
 
-reactDOM.render(<h1>Hello World</h1>, document.getElementById('app'));
+reactDOM.render(React.createElement(MeuComponente), document.getElementById('app'));
---
+++ app/components/MeuComponente.js
@@ -0,0 +1,12 @@
+var React = require('react'); // importa a lib react
+var createReactClass = require('create-react-class');
+
+var MeuComponente = createReactClass({
+  render: function(){
+    return (
+        <h1>Helloooo</h1>
+    );
+  }
+});
+
+module.exports = MeuComponente;
